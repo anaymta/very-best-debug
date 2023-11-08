1be86cec724fe94e7d12534a83f5bf9c39c77ac3
anaymta <74467992+anaymta@users.noreply.github.com>
2023-11-08 01:52:44 +0000

    2

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "users", :action => "home" })
   get("/users", { :controller => "users", :action => "all_users" })
-  get("/users/:username", { :controller => "user", :action => "show" })
-  post("/insert_user_record", { :controller => "venues", :action => "create" })
-  post("/update_users/:user_id", { :controller => "users", :action => "update" })
+  get("/users/:username", { :controller => "users", :action => "show" })
+  post("/insert_user_record/", { :controller => "users", :action => "create" })
+  post("/update_user/:user_id", { :controller => "users", :action => "update" })
 
   get("/venues", { :controller => "venues", :action => "index" })
-  post("/insert_venue_record", { :controller => "venues", :action => "create" })
-  get("/venue/:an_id", { :controller => "venue", :action => "show" })
+  get("/venues/:an_id", { :controller => "venues", :action => "show" })
   post("/update_venue/:the_id", { :controller => "application", :action => "update" })
+  post("/insert_venue_record", { :controller => "venues", :action => "create" })
   get("/delete_venue/:id_to_delete", { :controller => "venue", :action => "destroy" })
   
   post("/insert_comment_record", { :controller => "comments", :action => "create" })
